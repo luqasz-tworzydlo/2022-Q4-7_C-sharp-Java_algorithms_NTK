13495b946925ffe826235ee516cf6b98ba68f633
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-12-21 07:38:45 +0100

    Add files via upload

---
+++ T1_9-bubblesort_algorithm/src/bubblesort_algorithm/bubblesort_algorithm/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubblesort_algorithm
+{
+    // program wykonujący obliczenia przy wykorzystaniu
+    // algorytmu bąbelkowego [ czyli algorytm BubbleSort ]
+    // pomoc do egzaminu [użycie wcześniej przygotowanych algorytmów]
+    // => 'Algorytmy i struktury danych' [ Łukasz W. Tworzydło ]
+    internal class Program
+    {
+        public static int[] Our_Array = { 5, 7, 8, 3, 1, 4, 6, 9, 2 };
+        static void Main(string[] args)
+        {
+            BeforeASorting();
+
+            AfterAFirstSort();
+            AfterASecondSort();
+            AfterAThirdSort();
+
+            Console.WriteLine();
+            FinalResult();
+            Console.ReadKey();
+        }
+        static void BeforeASorting()
+        {
+            Console.WriteLine("\n=> Our unsorted list:");
+            foreach (int position in Our_Array)
+                Console.Write(position + " ");
+            Console.WriteLine();
+        }
+        static void FinalResult()
+        {
+            int qTemporary;
+            for (int jValue_Right = 0; jValue_Right <= Our_Array.Length - 2; jValue_Right++)
+            {
+                for (int iValue_Left = 0; iValue_Left <= Our_Array.Length - 2; iValue_Left++)
+                {
+                    if (Our_Array[iValue_Left] > Our_Array[iValue_Left + 1])
+                    {
+                        qTemporary = Our_Array[iValue_Left + 1];
+                        Our_Array[iValue_Left + 1] = Our_Array[iValue_Left];
+                        Our_Array[iValue_Left] = qTemporary;
+                    }
+                }
+            }
+            Console.WriteLine("\n=> Our sorted list [using BubbleSort]:");
+            foreach (int position in Our_Array)
+                Console.Write(position + " ");
+        }
+        static void AfterAFirstSort()
+        {
+            int qTemporary;
+            for (int jValue_Right = 0; jValue_Right <= 2 - 2; jValue_Right++)
+            {
+                for (int iValue_Left = 0; iValue_Left <= Our_Array.Length - 2; iValue_Left++)
+                {
+                    if (Our_Array[iValue_Left] > Our_Array[iValue_Left + 1])
+                    {
+                        qTemporary = Our_Array[iValue_Left + 1];
+                        Our_Array[iValue_Left + 1] = Our_Array[iValue_Left];
+                        Our_Array[iValue_Left] = qTemporary;
+                    }
+                }
+            }
+            Console.WriteLine("\n=> Our values after a first sort [BubbleSort]:");
+            foreach (int position in Our_Array)
+                Console.Write(position + " ");
+        }
+        static void AfterASecondSort()
+        {
+            int qTemporary;
+            for (int jValue_Right = 0; jValue_Right <= 2 - 2; jValue_Right++)
+            {
+                for (int iValue_Left = 0; iValue_Left <= Our_Array.Length - 2; iValue_Left++)
+                {
+                    if (Our_Array[iValue_Left] > Our_Array[iValue_Left + 1])
+                    {
+                        qTemporary = Our_Array[iValue_Left + 1];
+                        Our_Array[iValue_Left + 1] = Our_Array[iValue_Left];
+                        Our_Array[iValue_Left] = qTemporary;
+                    }
+                }
+            }
+            Console.WriteLine("\n=> Our values after a second sort [BubbleSort]:");
+            foreach (int position in Our_Array)
+                Console.Write(position + " ");
+        }
+        static void AfterAThirdSort()
+        {
+            int qTemporary;
+            for (int jValue_Right = 0; jValue_Right <= 2 - 2; jValue_Right++)
+            {
+                for (int iValue_Left = 0; iValue_Left <= Our_Array.Length - 2; iValue_Left++)
+                {
+                    if (Our_Array[iValue_Left] > Our_Array[iValue_Left + 1])
+                    {
+                        qTemporary = Our_Array[iValue_Left + 1];
+                        Our_Array[iValue_Left + 1] = Our_Array[iValue_Left];
+                        Our_Array[iValue_Left] = qTemporary;
+                    }
+                }
+            }
+            Console.WriteLine("\n=> Our values after a third sort [BubbleSort]:");
+            foreach (int position in Our_Array)
+                Console.Write(position + " ");
+        }
+    }
+}
